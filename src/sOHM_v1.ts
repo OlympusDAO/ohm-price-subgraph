e72da46dbfdb503f7a7c78d47bb6c45e97bbc2f8
Jem <0x0xjem@gmail.com>
2024-09-11 22:32:50 +0400

    Fix sOHM v0 index

--- src/sOHM_v1.ts
+++ src/sOHM_v1.ts
@@ -7,24 +7,14 @@ import { savePriceSnapshot } from "./priceSnapshot";
 import { SOHM_V0 } from "./constants";
 import { toDecimal } from "./dateHelper";
 
-const UINT256_MAX = BigInt.fromString(
-  "115792089237316195423570985008687907853269984665640564039457584007913129639935",
-);
-
 function getIndex(): BigDecimal {
   const sOhmContract = sOHM_v1.bind(Address.fromString(SOHM_V0));
 
-  const fragmentSupply = BigInt.fromI32(500000).times(
-    BigInt.fromI32(10).pow(9),
-  );
-  const totalGons = UINT256_MAX.minus(UINT256_MAX.mod(fragmentSupply));
   const totalSupply = sOhmContract.totalSupply();
-
-  log.info("total gons: ", [totalGons.toString()]);
-  log.info("total supply: ", [totalSupply.toString()]);
-  log.info("gons per fragment: ", [totalGons.div(totalSupply).toString()]);
-  // TODO finalise this
-  const index = toDecimal(totalSupply, 9).div(toDecimal(totalGons, 9));
+  // total supply / 5e14
+  const index = toDecimal(totalSupply, 9).div(
+    toDecimal(BigInt.fromU64(500000000000000), 0),
+  );
 
   return index;
 }
